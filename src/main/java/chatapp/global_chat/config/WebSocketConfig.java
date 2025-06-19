4534b594478ce9b51c601b3c9627d0505277acc5
Amardip Nikam <nikamamardip25@gmail.com>
2025-06-20 00:13:24 +0530

    Update WebSocketConfig.java for chat functionality

--- src/main/java/chatapp/global_chat/config/WebSocketConfig.java
+++ src/main/java/chatapp/global_chat/config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package chatapp.global_chat.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,15 +12,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
     }
+    
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
                 .setAllowedOriginPatterns("*") // Allow all origins with pattern matching
                 .withSockJS();
